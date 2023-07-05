fed86ffcfe1876657e48191f4e5b856a4640f0d4
khizzioui97 <khiziouii@gmail.com>
2023-07-05 07:53:39 -0700

    101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """the N-queens puzzle solved."""
 
+
 import sys
 
 
+
 def init_board(n):
-    """Initializing an `n`x`n` sized chessboard with 0's."""
+    """Initializing an `n `x `n ` sized chessboard with 0's."""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
     return (board)
+
 
 
 def board_deepcopy(board):
@@ -17,6 +20,7 @@ def board_deepcopy(board):
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
+
 
 
 def get_solution(board):
