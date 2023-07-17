0d8255c75c814399d0a8c535ea736c07944fa3b4
khizzioui97 <khiziouii@gmail.com>
2023-07-17 16:40:27 -0700

    task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""Defines a base model class."""
+
 
 class Base:
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is None:
+        """Initialize a new Base.
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else:
-            self.id = id
